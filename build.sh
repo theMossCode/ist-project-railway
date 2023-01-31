5c7a6ac0e58b3ff825e5b7295239168f04b0eb3e
theMossCode <collombuvi@gmail.com>
2023-01-31 19:02:53 +0300

    added lock cmd

--- build.sh
+++ build.sh
@@ -3,11 +3,9 @@
 set -o errexit
 
 echo "$(poetry --version)"
-curl -sSL https://install.python-poetry.org | python3 - --uninstall
-curl -sSL https://install.python-poetry.org | python3 -
-poetry self update
-#poetry update
-#poetry install
+poetry lock
+poetry update
+poetry install
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
