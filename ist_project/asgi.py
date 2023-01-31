180f8a3bbedacafe7d04f2458e5fdb150cdacbd1
theMossCode <collombuvi@gmail.com>
2023-01-31 21:41:24 +0300

    deployment updates

--- ist_project/asgi.py
+++ ist_project/asgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from django.core.asgi import get_asgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ist_project.settings')
 
-from django.core.asgi import get_asgi_application
 from channels.routing import ChannelNameRouter, ProtocolTypeRouter
 from dashboard.mqtt.consumers import MQTTConsumer
 
