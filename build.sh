158b83d9f940cc8535d46875886313a8c99a8846
theMossCode <collombuvi@gmail.com>
2023-01-31 18:31:01 +0300

    build.sh updates

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -o errexit
 
 echo "$(poetry --version)"
 poetry self install --sync
-poetry self update --dry-run
-poetry update
-poetry install
+#poetry self update --dry-run
+#poetry update
+#poetry install
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
