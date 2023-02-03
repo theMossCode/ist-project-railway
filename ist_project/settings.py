918b394731d71669046f5791a031fea396021f20
theMossCode <collombuvi@gmail.com>
2023-02-03 14:51:45 +0300

    added Procfile, lock file and runtime.txt

--- ist_project/settings.py
+++ ist_project/settings.py
@@ -159,7 +159,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
